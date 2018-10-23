0017c50c54ebbdbfc1d9a9c86a870c2a3c7d1ac3
rytwalker <rytwalker@gmail.com>
2018-10-23 18:06:14 -0400

    Updates

---
+++ precache-manifest.0352d3482b12795a63ac33e2f4f151cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5af0434416c61083be22",
+    "url": "/Redux-Todo/do-work-redux/static/js/runtime~main.5af04344.js"
+  },
+  {
+    "revision": "d53469562e486ed15f65",
+    "url": "/Redux-Todo/do-work-redux/static/js/main.d5346956.chunk.js"
+  },
+  {
+    "revision": "6de31c29a4fd2f8056df",
+    "url": "/Redux-Todo/do-work-redux/static/js/1.6de31c29.chunk.js"
+  },
+  {
+    "revision": "d53469562e486ed15f65",
+    "url": "/Redux-Todo/do-work-redux/static/css/main.a0c7bad2.chunk.css"
+  },
+  {
+    "revision": "39a8671565f330d225059bcc05f935df",
+    "url": "/Redux-Todo/do-work-redux/index.html"
+  }
+];
